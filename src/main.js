7ad5b15bc2ca8ebc66d76b60723d549a8a02f80e
edmirgalvan01 <edmirgalvaz@gmail.com>
2022-05-17 14:25:04 -0500

    Se ven los productos correctamente

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 const URL_CATEGORIES = 'https://api.escuelajs.co/api/v1/categories';
+const URL_ALL_PRODUCTS =
+   'https://api.escuelajs.co/api/v1/products?offset=0&limit=10';
 
 async function getAllCategories() {
    const response = await fetch(URL_CATEGORIES);
@@ -24,4 +26,40 @@ async function getAllCategories() {
    });
 }
 
+async function getAllProducts() {
+   const response = await fetch(URL_ALL_PRODUCTS);
+   const data = await response.json();
+
+   productsListContainer.innerText = '';
+   data.map((product) => {
+      const card = document.createElement('div');
+      const img = document.createElement('img');
+      const info = document.createElement('div');
+      const title = document.createElement('p');
+      const price = document.createElement('p');
+
+      card.classList.add('product');
+      img.classList.add('product--img');
+      info.classList.add('product--info');
+      title.classList.add('product--info__title');
+      price.classList.add('product--info__price');
+
+      title.innerText = product.title;
+      price.innerText = `$${product.price}`;
+      info.appendChild(title);
+      info.appendChild(price);
+      img.setAttribute('src', product.images[0]);
+      img.setAttribute('alt', product.title);
+      card.appendChild(img);
+      card.appendChild(info);
+
+      productsListContainer.appendChild(card);
+
+      card.addEventListener('click', () => {
+         console.log('Holaaa');
+      });
+   });
+}
+
+getAllProducts();
 getAllCategories();
--- src/nodes.js
+++ src/nodes.js
@@ -2,3 +2,6 @@ const $ = (id) => document.querySelector(id);
 
 // CATEGORIES
 const categoriesListContainer = $('.categories--list');
+
+// PRODUCTS
+const productsListContainer = $('.products--list');
